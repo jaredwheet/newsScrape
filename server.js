1dfcc180ef7f4042f4aed9e450c8cf5fca4fedd6
Jared Wheet <jaredwheet@gmail.com>
2019-02-19 16:56:56 -0700

    mongoose connections updated

--- server.js
+++ server.js
@@ -32,7 +32,11 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
+
+// mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI), { useNewUrlParser: true };
+
 
 // Routes
 
